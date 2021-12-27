e70ca334b5ea4da02cfea750ad67fbf0837b67b9
Aakash Patel <aakashpatel@Aakashs-MacBook-Pro.local>
2021-12-27 13:11:53 -0500

    First Commit

---
+++ tests/test_script.py
@@ -0,0 +1,24 @@
+import unittest
+
+from main import app
+import os
+
+
+class TestToPerform(unittest.TestCase):
+    def setUp(self):
+        pass
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        print(response)
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
